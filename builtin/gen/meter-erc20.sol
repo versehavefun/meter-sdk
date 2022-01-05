b2c4dff990859f01c5bf47d99f961ff0447f5077
kda-data <kda-data@protonmail.com>
2022-01-05 08:40:26 +0800

    update meter-erc20.sol

--- builtin/gen/meter-erc20.sol
+++ builtin/gen/meter-erc20.sol
@@ -11,7 +11,7 @@ import "./imeternative.sol";
 contract MeterERC20 is _Token {
     mapping(address => mapping(address => uint256)) allowed;
 
-    IMeterNative _meterTracker = IMeterNative(0x0000000000004E65774D657465724E6174697665);
+    IMeterNative constant _meterTracker = IMeterNative(0x0000000000004E65774D657465724E6174697665);
 
     function name() public pure returns(string) {
         return "STP Token";
@@ -82,7 +82,7 @@ contract MeterERC20 is _Token {
 contract MeterGovERC20 is _Token {
     mapping(address => mapping(address => uint256)) allowed;
 
-    IMeterNative _meterTracker = IMeterNative(0x0000000000004E65774D657465724E6174697665);
+    IMeterNative constant _meterTracker = IMeterNative(0x0000000000004E65774D657465724E6174697665);
 
     function name() public pure returns(string) {
         return "Verse Network";
